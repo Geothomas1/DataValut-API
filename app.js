2cf814c8fcdf6ae0cd8886ff632dbf9cb95ecad3
geothomas <geothomas@cet.ac.in>
2020-12-22 22:07:56 +0530

    connection,helper,bycrypt....

--- app.js
+++ app.js
@@ -140,11 +140,6 @@ app.get('/logout', (req, res) => {
 
 
 
-
-
-
-
-
 app.post('/users', async function(req, res) {
     var username = req.body.username;
     var orgName = req.body.orgName;
---
+++ helpers/user-helper.js
@@ -0,0 +1,7 @@
+var db = require('../config/connection')
+var collection = require('../config/collections')
+const bcrypt = require('bcrypt')
+var objectId = require('mongodb').ObjectID
+module.exports = {
+
+}
