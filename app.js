84d48b9f65aa66998fb72476761680e6fb30ae69
geothomas <geothomas@cet.ac.in>
2021-01-01 10:52:33 +0530

    app.use middle for JWT token

--- app.js
+++ app.js
@@ -209,33 +209,37 @@ app.use(expressJWT({
 }).unless({
     path: ['/users', '/metrics']
 }));
-app.use(bearerToken());
-app.use(function(req, res, next) {
-    logger.debug(' ------>>>>>> new request for %s', req.originalUrl);
-    if (req.originalUrl.indexOf('/users') >= 0 || req.originalUrl.indexOf('/metrics') >= 0) {
-        return next();
-    }
 
-    var token = req.token;
-    jwt.verify(token, app.get('secret'), function(err, decoded) {
-        if (err) {
-            res.send({
-                success: false,
-                message: 'Failed to authenticate token. Make sure to include the ' +
-                    'token returned from /users call in the authorization header ' +
-                    ' as a Bearer token'
-            });
-            return;
-        } else {
-            // add the decoded user name and org name to the request object
-            // for the downstream code to use
-            req.username = decoded.username;
-            req.orgname = decoded.orgName;
-            logger.debug(util.format('Decoded from JWT token: username - %s, orgname - %s', decoded.username, decoded.orgName));
-            return next();
-        }
-    });
-});
+
+// app.use(bearerToken());
+// app.use(function(req, res, next) {
+//     logger.debug(' ------>>>>>> new request for %s', req.originalUrl);
+//     if (req.originalUrl.indexOf('/users') >= 0 || req.originalUrl.indexOf('/metrics') >= 0) {
+//         return next();
+//     }
+
+//     var token = req.token;
+//     jwt.verify(token, app.get('secret'), function(err, decoded) {
+//         if (err) {
+//             res.send({
+//                 success: false,
+//                 message: 'Failed to authenticate token. Make sure to include the ' +
+//                     'token returned from /users call in the authorization header ' +
+//                     ' as a Bearer token'
+//             });
+//             return;
+//         } else {
+//             // add the decoded user name and org name to the request object
+//             // for the downstream code to use
+//             req.username = decoded.username;
+//             req.orgname = decoded.orgName;
+//             logger.debug(util.format('Decoded from JWT token: username - %s, orgname - %s', decoded.username, decoded.orgName));
+//             return next();
+//         }
+//     });
+// });
+
+
 
 // // Create Channel
 // app.post('/channels', async function(req, res) {
